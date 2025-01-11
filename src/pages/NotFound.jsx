cc27ce5ef2140631e3fa108a28200f25d9ef1526
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2025-01-11 19:30:05 +0500

    New Commit

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const NotFound = () => {
   return (
-    <div className='flex justify-center items-center bg-slate-800 h-[100vh]'>
+    <div className='flex justify-center items-center bg-slate-900 h-[100vh]'>
       <h1 className='text-[2.5rem] text-white font-semibold uppercase'><span className='text-red-600'>!404</span> Page not found</h1>
     </div>
   )
